9e9de6e0b0f6ac5404b0c84ae5192a0dd3912901
MondaleFelix <mondalefelix@yahoo.com>
2024-07-16 13:21:01 -0700

    Created calculator

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Mondale!");
+
+// Declare a (string) variable
+string myFriendsName;
+
+
+// Assign a value to myFriendsName variable
+myFriendsName = "Mondale";
+
+// Use/access the variable
+Console.WriteLine(myFriendsName);
+
+// Overwriting the variable value
+myFriendsName = "Lemon";
+
+Console.WriteLine(myFriendsName);
+
+Console.ReadKey();
+
+
+string myFriendsName2 = "Eggshells";
+
+Console.WriteLine(myFriendsName2);
+
+myFriendsName2 = "I eat eggshells";
+
+
+Console.WriteLine(myFriendsName2);
---
+++ MyAdditionCalculator/MyAdditionCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Prints inside whatever inside of ()
+Console.WriteLine("Enter a whole number!");
+
+
+
+
+// Take the user input and store it
+// Variable with the name "userInput" and the data type "string"
+// strings are used for text
+string userInput = Console.ReadLine();
+
+int myNumber = int.Parse(userInput);
+
+Console.WriteLine("Enter a whole number!");
+
+userInput = Console.ReadLine();
+
+int myNumber2 = int.Parse(userInput);
+
+
+
+
+
+Console.WriteLine("The sum is " +  (myNumber + myNumber2));
+Console.ReadKey();
